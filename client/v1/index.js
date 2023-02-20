3634a8ee8a07b8f39e5e9cc8ccd1b0af9835d254
Timvtl <timothe.vital@gmail.com>
2023-02-20 19:41:39 +0100

    TODO 3

--- client/v1/index.js
+++ client/v1/index.js
@@ -61,10 +61,23 @@ console.log(nbProducts);
 // 2. Log the variable
 // 3. Log how many brands we have
 
+var listBrand = [];
+
+for(var i = 0; i < marketplace.length; ++i){
+  if(listBrand.includes(marketplace[i].brand) == false)
+  listBrand = listBrand + " " + marketplace[i].brand;
+}
+
+console.log(listBrand);
+
+
 // 🎯 TODO 4: Sort by price
 // 1. Create a function to sort the marketplace products by price
 // 2. Create a variable and assign it the list of products by price from lowest to highest
 // 3. Log the variable
+
+
+
 
 // 🎯 TODO 5: Sort by date
 // 1. Create a function to sort the marketplace objects by products date
