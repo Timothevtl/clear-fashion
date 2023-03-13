14e8ecd7d57d748edf067d5839b24490ba9f8588
Timvtl <timothe.vital@gmail.com>
2023-03-13 18:01:48 +0100

    oui

---
+++ server/eshops/Montlimart.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const fs =require('fs');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.product-list .products-list__block')
+    .map((i, element) => {
+      const name = $(element)
+        .find('product-miniature__color')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+      const price = parseInt(
+        $(element)
+          .find('.price')
+          .text()
+      );
+
+      return {name, price};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+      const products = parse(body);
+
+      // Write the products to a JSON file
+      fs.writeFileSync('productsmonlimart.json', JSON.stringify(products, null, 2));
+
+      return products;
+    }
+    
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
